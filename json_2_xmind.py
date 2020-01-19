e8b6574c070a19e73dad4490601e58fa9ac46baf
zhaojinzhou <791620078@qq.com>
2020-01-19 18:50:55 +0800

    Create json_2_xmind.py

---
+++ json_2_xmind.py
@@ -0,0 +1,56 @@
+import xmind
+import json
+import sys
+import os
+
+def dfs(topic_data, topic):
+    if str(type(topic_data)) == "<class 'dict'>":
+        for i in topic_data.keys():
+            sub_topic = topic.addSubTopic()
+            sub_topic.setTitle(i)
+            dfs(topic_data[i], sub_topic)
+    else:
+        if str(type(topic_data)) == "<class 'list'>":
+            data=''
+            index = 0
+            for i in topic_data:
+                if(str(type(i)) == "<class 'dict'>"):
+                    sub_topic = topic.addSubTopic()
+                    sub_topic.setTitle(index)
+                    index += 1
+                    dfs(i, sub_topic)
+                    continue
+                data += i
+                data += ','
+            if(len(data) != 0):
+                sub_topic = topic.addSubTopic()
+                sub_topic.setTitle(data)
+        else:
+            sub_topic = topic.addSubTopic()
+            sub_topic.setTitle(topic_data)
+            return sub_topic
+
+
+def json_2_xmind(json_file_path, xmind_name):
+    workbook = xmind.load(xmind_name)
+    sheet1 = workbook.getPrimarySheet()
+    sheet1.setTitle(xmind_name)  # 设置画布名称
+    root_topic1 = sheet1.getRootTopic()
+    root_topic1.setTitle(xmind_name)
+    with open(json_file_path) as f:
+        dict_data = json.load(f)
+        dfs(dict_data, root_topic1)
+    xmind.save(workbook, path=xmind_name)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print('''
+        usage :
+            python3 json_2_xmind.py exp.json name.xmind
+        ''')
+        exit()
+    if not os.path.exists(sys.argv[1]):
+        print("file", sys.argv[1], "not exist")
+        exit()
+    json_2_xmind(sys.argv[1], sys.argv[2])
